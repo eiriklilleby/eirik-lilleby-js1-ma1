4c1e98cb367527750ed6d27eabd18d9de2fe82c8
eiriklilleby <71594873+eiriklilleby@users.noreply.github.com>
2021-01-22 17:58:20 +0100

    Added files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# eirik-lilleby-js1-ma1
+# Files for JavaScript 1 Module Asssignment 1
---
+++ js/script.js
@@ -0,0 +1,39 @@
+const cats = [
+  {
+    name: "Blob",
+    age: 10,
+  },
+  {
+    name: "Harold",
+  },
+  {
+    name: "Blurt",
+    age: 21,
+  },
+];
+
+// QUESTION 1
+
+const cat = "complain";
+
+function talk(cat) {
+  console.log(cat);
+}
+
+talk("Meow!");
+
+// Answer: "Meow!"
+
+// QUESTION 2
+
+heading = document.querySelector("h3");
+
+heading.innerHTML = `Updated Subheading`;
+
+// Question 3
+
+heading.style.fontSize = "2em";
+
+// Question 4
+
+heading.classList.add("Subheading");
